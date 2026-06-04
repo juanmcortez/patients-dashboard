6e80c77187b99b70d66c243c1fa4408c6894676f
Juan M. Cortéz <juanm.cortez@gmail.com>
2026-06-04 09:55:32 -0300

    🚧 Customizing the UI base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/patients-dashboard.css', 'resources/js/patients-dashboard.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
